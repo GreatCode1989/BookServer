f02f78a80b98b9754fe24d268927bbd7a3f350bb
GreatCode1989 <wef666wef6@gmail.com>
2024-02-25 18:33:17 +0300

    lo

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
-// Ваш файл bootstrap.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Используем process.env.PORT, чтобы получить порт из среды выполнения
+  // Настройка CORS
+  const corsOptions: CorsOptions = {
+    origin: ['http://localhost:5173'], // Укажите свой фронтенд-домен
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204,
+  };
+  app.enableCors(corsOptions);
+
   const port = process.env.PORT || 3000;
 
   await app.listen(port);
