1c4093a3fe4676e842dcc0fe0f573dfd760705a4
GreatCode1989 <wef666wef6@gmail.com>
2024-02-25 12:52:09 +0300

    lo

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
+// Ваш файл bootstrap.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // Используем process.env.PORT, чтобы получить порт из среды выполнения
+  const port = process.env.PORT || 3000;
+
+  await app.listen(port);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
+
 bootstrap();
